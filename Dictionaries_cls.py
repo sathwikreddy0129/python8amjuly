babb8bf1243fe7c0d1be2481eed3cbc0f1fb3900
sathwikreddy0129 <reddy90sathwik@gmail.com>
2020-09-18 12:06:27 +0530

    Adding the new file first time

---
+++ Dictionaries_cls.py
@@ -0,0 +1,101 @@
+# a ={ 'username':'sathwik', 'password':'sanjev'}
+# print(a)
+# print(type(a))
+
+#a = {'username': 'sanjev', ('mobile'):9898565645, 1:45} -- any data type for dictionary
+# a = {'username': 'sanjev', ('mobile'):9898565645, 1:45} 
+# print(a['password'])
+# print(a[1])
+# print(a[('mobile')])
+# a['email'] = 'gsajev@gmail.com'
+# print(a)
+
+#nested dictioaries
+# a = {"india" : {"telangana": "hyderabad", "tamiladu":"chennai"},
+#     "usa" : {"newyork" : "newyork", "california" : "california"}}
+# print(a["india"])
+# print(a["india"] ["telangana"])
+# print(a["usa"] ["newyork"])
+
+# dictionary methods
+
+#get
+# a = {"continent" :"africa"}
+# print(a.get("africa"))
+# print(a)
+
+#update
+
+# a = {"username" : "sanjeev", "password" : "sajeev123"}
+# b = {"email" : "sanjeev@gmail.com", "phone" : "9959452655"}
+# a.update(b)
+# print(a)
+# a['city'] = "hyderabad"
+# print(a)
+
+
+#pop
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# a.pop('password')
+# print(a)
+
+#popitem
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# a.popitem( )
+# print(a)
+
+#clear
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# a.clear()
+# print(a)
+
+#keys
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# print(a.keys())
+
+#value
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# print(a.values())
+
+#items
+# a = {"username" : "sanjeev", 'password' : "sajeev123"}
+# print(a.items())
+
+#fromkeys()
+# a = ['username', 'password', 'email']
+# print({}. fromkeys(a))
+# print({}. fromkeys(a,12))
+
+#setdefault
+# a = {'username':'sanjeev', 'password':'san'}
+# a.setdefault('phone')
+# print(a)
+# a['phone'] = none 
+# a.setdefault('phone',7894561235)
+# print(a)
+# a['phone'] = 1597846325
+# print(a)
+
+#copy
+# a = {'username':'sanjeev', 'password': 'sanjeev123'}
+# b = a.copy()
+# b = a
+# print(a)
+# print(b)
+
+# a['phone'] = 7893214565
+# print(a)
+# print(b)
+
+#dictionary comprehension
+
+#1st way of syntax
+# a = [3,8,3,6,4,1]
+# print({j:j*2 for j in [3,8,3,6,4,1]})
+
+#2nd way syntax
+# print({{row : a[row] for row in [3,8,3,6,4,1]if row in ['username', 'password', 'email']}})
+
+#3rd way syntax
+print({j:j*2 if j%2==0 else j*3 for j in [3,8,3,6,4,1]})
+
---
+++ list_cls.py
@@ -0,0 +1,104 @@
+a = [23,45.67, 'python',34,"django"]
+# print(type(a))
+# print(a[0])
+# print(a[4])
+# print(a[1:5])
+# print(a[-1:-4:-1])
+
+# a = [23, 45.67, 'python', 34, "django"]
+# print(a)
+# a[1] = 76
+# print(a)
+# a[1:4] = [78,"datascience",67.43]
+# print(a)
+# del a[2]
+# print(a)
+
+
+#list methods
+
+#append
+# a = [34,'python','django',56]
+# print(a)
+# a.append('datascience')
+# print(a)
+# a.append([11,12,13])
+# print(a)
+
+#extend 
+# a = [34,'python','django',56]
+# a.extend('django')
+# a.extend([11,12,13])
+# print(a)
+
+#insert
+# a = [34,'python',"django",56]
+# a.insert(2,45)
+# a.insert(2,"datascience")
+# a.insert(2,[45,46])
+# print(a)
+
+#remove 
+# a = [34,'python',"django",56]
+# a.remove('python')
+# print(a)
+
+#pop
+# a = [34,'python',"django",56]
+# a.pop(3)
+# print(a)
+
+#clear
+# a = [34,'python',"django",56]
+# a.clear()
+# print(a)
+
+#nested indexing
+# a = [23, [11,12,13],"django", [51,52,53]]
+# print(a[1][2])
+# a[3].append(54)
+# print(a)
+
+#index
+# a = [23, [11,12,13],"django", [51,52,53]]
+# print(a.index('django'))
+
+#count
+# a = [23, [11,12,13],"django", [51,52,53]]
+# print(a.count('django'))
+# print(a.count('djan'))
+
+#reverse
+# a = [23, [11,12,13],"django", [51,52,53]]
+# a.reverse()
+# print(a)
+
+#sort
+# a = [34,24,67,54,89,54,75,12]
+# a.sort()
+# print(a)
+# print(len(a))
+
+# #looping example
+
+# a = [64,63,82,54,96,79,32,55,65,96,31]
+# even = []
+# odd = []
+# for j in a:
+#     if j%2==0:
+#         # print("even",j)
+#         even.append(j)
+#     else:
+#         odd.append(j)
+# print(even)
+# print(odd)
+
+
+#list comprehension
+
+#1st way syntax
+print([j*2 for j in [3,4,5,6,7]])
+#2nd way of syntax
+print([ j*2 for j in [3,4,5,6,7] if j%2==1])
+#3rd way of syntax
+print([j*2 if j%2==1 else j*4 for j in [3,4,5,6,7]])
